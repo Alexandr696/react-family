706f2251d3a129832f22807098fb8d2fe57c068b
Alexander <52827777+Alexandr696@users.noreply.github.com>
2023-02-17 20:50:06 +0600

    html

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <table className="responsive-table">
             <thead>
                 <tr>
-                    <th>Column 1</th>
+                    <th>#</th>
                     <th>Column 2</th>
                     <th>Column 3</th>
                     <th>Column 4</th>
@@ -20,26 +20,26 @@ function App() {
             </thead>
             <tbody>
                 <tr>
-                    <td data-label="Column 1">Data 1Data 1Data 1Data 1</td>
-                    <td data-label="Column 2">Data 2</td>
-                    <td data-label="Column 3">Data 3</td>
-                    <td data-label="Column 4">Data 4</td>
-                    <td data-label="Column 5">Data 5</td>
-                    <td data-label="Column 6">Data 6</td>
-                    <td data-label="Column 7">Data 7</td>
-                    <td data-label="Column 8">Data 8</td>
-                    <td data-label="Column 9">Data 9</td>
+                    <td data-label="#">1</td>
+                    <td data-label="Имя">Милана</td>
+                    <td data-label="Красота">10 из 10</td>
+                    <td data-label="Ум">10 из 10</td>
+                    <td data-label="Сексуальность">10 из 10</td>
+                    <td data-label="Дурацкие вещи">нет</td>
+                    <td data-label="Запах">Ароматный</td>
+                    <td data-label="Пердит">нет</td>
+                    <td data-label="Воняет">нет</td>
                 </tr>
                 <tr>
-                    <td data-label="Column 1">Data 10Data 10Data 10Data 10</td>
-                    <td data-label="Column 2">Data 11</td>
-                    <td data-label="Column 3">Data 12</td>
-                    <td data-label="Column 4">Data 13</td>
-                    <td data-label="Column 5">Data 14</td>
-                    <td data-label="Column 6">Data 15</td>
-                    <td data-label="Column 7">Data 16</td>
-                    <td data-label="Column 8">Data 17</td>
-                    <td data-label="Column 9">Data 18</td>
+                  <td data-label="#">2</td>
+                    <td data-label="Имя">Саша</td>
+                    <td data-label="Красота">5 из 10</td>
+                    <td data-label="Ум">10 из 10</td>
+                    <td data-label="Сексуальность">3 из 10</td>
+                    <td data-label="Дурацкие вещи">Все</td>
+                    <td data-label="Запах">Противный</td>
+                    <td data-label="Пердит">Да</td>
+                    <td data-label="Воняет">Да</td>
                 </tr>
             </tbody>
         </table>  
