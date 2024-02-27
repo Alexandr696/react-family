60f43ac61f4174997d0cb097425cb2b14092aa21
Alexandr696 <akado_2000@mail.ru>
2024-02-27 11:14:37 +0600

    transform

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -59,7 +59,7 @@ export const Header: FC<ShowModelProps> = ({ ShowModel }: ShowModelProps) => {
                           className={ClassNames(
                             item.current
                               ? 'bg-gray-900 text-white'
-                              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                              : 'text-gray-300 transition-colors hover:bg-gray-700 hover:text-white',
                             'rounded-md px-3 py-2 text-sm font-medium'
                           )}
                           aria-current={item.current ? 'page' : undefined}
